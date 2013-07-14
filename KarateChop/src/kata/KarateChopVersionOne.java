e3c5a95de2fe1ece9605a7aa7d58820bec4b8971
Ruxandra <ruxandra.barz@gmail.com>
2013-07-14 17:35:57 +0300

    KarateChop - version one

---
+++ KarateChop/src/kata/KarateChopVersionOne.java
@@ -0,0 +1,22 @@
+package kata;
+
+public class KarateChopVersionOne {
+
+	public int chop(int searchFor, int[] array) {
+
+		int low = 0;
+		int high = array.length - 1;
+		while (low <= high) {
+			int index = ((high - low) / 2) + low;
+			if (array[index] == searchFor)
+				return index;
+			else if (array[index] < searchFor) {
+				low = index + 1;
+			} else if (array[index] > searchFor) {
+				high = index - 1;
+			}
+
+		}
+		return -1;
+	}
+}
---
+++ KarateChop/src/tests/TestKarateChopVersionOne.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import kata.KarateChopVersionOne;
+
+public class TestKarateChopVersionOne {
+
+	@Test
+	public void testChopEmptyArray() {
+		KarateChopVersionOne karateChop = new KarateChopVersionOne();
+		assertEquals(-1, karateChop.chop(-1, buildArray()));
+		assertEquals(0, karateChop.chop(1, buildArray(1)));
+		assertEquals(1, karateChop.chop(2, buildArray(1, 2)));
+		
+		assertEquals(0, karateChop.chop(1, buildArray(1, 3, 5)));
+		assertEquals(1, karateChop.chop(3, buildArray(1, 3, 5)));
+		assertEquals(2, karateChop.chop(5, buildArray(1, 3, 5)));
+		
+	}
+
+	private int[] buildArray(int ... values) {
+		return values;
+	}
+
+//	@Test
+//	public void testChopArrayNotContainingValue() {
+//		KarateChopVersionOne karateChop = new KarateChopVersionOne();
+//		assertEquals(-1, karateChop.chop(2, new int[] { 1, 3, 5 }));
+//	}
+}
